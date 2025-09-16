701432421ff2f3f8d75f2fcaa55cb1548c7256e2
smalakargh <supriyomic@gmail.com>
2025-09-16 22:05:37 +0530

    files #Updated

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,12 +1,37 @@
+'use client';
 import Link from "next/link";
+import { motion } from "framer-motion";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-indigo-500 to-pink-500 font-poppins px-4">
       <nav className="flex gap-4">
-          <Link href="/signup" className="bg-amber-50 flex px-4 py-2 rounded-lg text-black ">Sign Up</Link>
-          <Link href="/signin" className="bg-amber-50 flex px-4 py-2 rounded-lg text-black">Sign In</Link>
-        </nav>
+        <motion.div
+          whileHover={{ scale: 1.05 }}
+          whileTap={{ scale: 0.95 }}
+          transition={{ type: "spring", stiffness: 300 }}
+        >
+          <Link
+            href="/signup"
+            className="bg-amber-50 flex px-4 py-2 rounded-lg text-black shadow-md"
+          >
+            Sign Up
+          </Link>
+        </motion.div>
+
+        <motion.div
+          whileHover={{ scale: 1.05 }}
+          whileTap={{ scale: 0.95 }}
+          transition={{ type: "spring", stiffness: 300 }}
+        >
+          <Link
+            href="/signin"
+            className="bg-amber-50 flex px-4 py-2 rounded-lg text-black shadow-md"
+          >
+            Sign In
+          </Link>
+        </motion.div>
+      </nav>
     </div>
   );
 }
